12010698599db412bf5bc0d1cc49701ee63542fd
prajwalppramod <prajwalppramod@gmail.com>
2025-02-27 16:39:45 +0530

    initial commit

---
+++ src/main/java/com/carrentalapp/carrentalapp/CarrentalappApplication.java
@@ -0,0 +1,13 @@
+package com.carrentalapp.carrentalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrentalappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrentalappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carrentalapp/carrentalapp/CarrentalappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrentalapp.carrentalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrentalappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
