fbb07031f4e79513407a050b5b3ed23b8c1af305
Anna-Biju-Abraham <annabijuabraham2019@gmail.com>
2025-02-28 01:25:20 +0530

    viewBookings committed

--- src/main/java/com/training/CarRentalApp/Repositories/BookingsRepository.java
+++ src/main/java/com/training/CarRentalApp/Repositories/BookingsRepository.java
@@ -12,5 +12,6 @@ import com.training.CarRentalApp.Models.Bookings;
 
 public interface BookingsRepository extends JpaRepository<Bookings, Long>
 {
-	
+    @Query(value = "SELECT b.booking_id, u.username, c.car_name, b.from_date, b.to_date, b.total_amount FROM Bookings b JOIN Users u ON b.user_id = u.user_id JOIN Cars c ON b.car_id = c.car_id", nativeQuery = true)
+    List<Map<String, String>> getAllBookings();
 }
